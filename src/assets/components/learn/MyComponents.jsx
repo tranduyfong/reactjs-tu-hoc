1397ca53ccc4e230c7a5daf170563658fb208446
tranduyfong <tranphong86958@gmail.com>
2025-06-25 23:02:53 +0700

    Video 30

--- src/assets/components/learn/MyComponents.jsx
+++ src/assets/components/learn/MyComponents.jsx
@@ -1,9 +1,20 @@
+// const MyComponent = () => {
+//     return (
+//         <div>Tran Duy Phong</div>
+//         <div>Error</div>
+//     );
+// }
+//           LOI DO NO CHi TRA VE 1 PARENT TRONG RETURN
+
+import './style.css';
+
+// FRAGMENT
 const MyComponent = () => {
     return (
-        <div>
-            Tran Duy Phong
-        </div>
+        <>
+            <div className="abc">Tran Duy Phong</div>
+            <div className="child">Child</div>
+        </>
     );
 }
-
 export default MyComponent;
