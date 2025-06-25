568983de07b772a07ea202078eb7cb87b092e207
tranduyfong <tranphong86958@gmail.com>
2025-06-25 23:30:34 +0700

    Video 31

--- src/assets/components/learn/MyComponents.jsx
+++ src/assets/components/learn/MyComponents.jsx
@@ -10,9 +10,15 @@ import './style.css';
 
 // FRAGMENT
 const MyComponent = () => {
+    // const temp = 'Toi ten la: '; // string
+    // const arrayNumber = [1, 2, 3, 4]
+    const person = {
+        name: 'Tran Duy Phong',
+        age: 23
+    }
     return (
         <>
-            <div className="abc">Tran Duy Phong</div>
+            <div className="abc">{JSON.stringify(person)}Tran Duy Phong</div>
             <div className="child">Child</div>
         </>
     );
