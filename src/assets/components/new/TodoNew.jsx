453e6429a4b533d57bf65cca477bf93a18a0b122
tranduyfong <tranphong86958@gmail.com>
2025-08-02 20:44:59 +0700

    video 36

--- src/assets/components/new/TodoNew.jsx
+++ src/assets/components/new/TodoNew.jsx
@@ -4,10 +4,21 @@ const TodoNew = (props) => {
     console.log(props);
     const { addNewTodo } = props;
     // addNewTodo('duy phong');
+    const handleClick = () => {
+        alert('click me')
+    }
+    const handleOnChange = (name) => {
+        console.log('>>>>>>>>>>> handleOnChange', name)
+    }
     return (
         <div className="todo-new">
-            <input type="text" />
-            <button>Add</button>
+            <input type="text"
+                onChange={(event) => handleOnChange(event.target.value)}
+            />
+            <button
+                style={{ cursor: 'pointer' }}
+                onClick={handleClick}
+            >Add</button>
         </div>
     );
 }
